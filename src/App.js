1a0829e4e3ccadabe01977632494811444c8cfb3
jozzydgr8 <jozzydgreat1@gmail.com>
2024-08-25 05:31:01 -0700

    welcome to environment

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome this is where you will track changes and progress made for this website.</h1>
+    </div>
+  );
+}
+
+export default App;

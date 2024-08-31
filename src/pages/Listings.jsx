de6b2529415ffad2a492cbbdfae5093aaf0e9c3d
jozzydgr8 <jozzydgreat1@gmail.com>
2024-08-31 09:58:46 -0700

    grid fix

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -6,13 +6,14 @@ function Listings() {
   return (
     <section>
         <div className='container-fluid'>
-        <h1>
+            
+        <h1 >
             Our Apartments
         </h1>
-        <div className='row'>
-            <div className='col-md-5 list-grid'>
+        <div className='listRow'>
+            <div className='list-grid'>
             
-                    <h3>3 apartmen duplex</h3>
+                    <h3 style={{padding:'0 2%'}}>3 bedroom duplex</h3>
                     <div className='gridProduct row'>
 
                         <div className='list-image col-md-6'>
@@ -20,11 +21,9 @@ function Listings() {
                         </div>
 
                         <div className='col-md-6 listing-details'>
-                        <p className=''>
-                            Lorem ipsum
-                             dolor sit amet consectetur, adipisicing elit.
-                            A, fugiat?
-                        </p>
+                            <h3>
+                                #3,500,000
+                            </h3>
                         </div>
 
                     </div>
@@ -43,9 +42,9 @@ function Listings() {
 
 
 
-            <div className='col-md-5 gap list-grid'>
+            <div className='list-grid'>
             
-                    <h3>3 apartmen duplex</h3>
+                    <h3 style={{padding:'0 2%'}}>2 bedroom duplex apartment</h3>
                     <div className='gridProduct row'>
 
                         <div className='list-image col-md-6'>
@@ -53,11 +52,9 @@ function Listings() {
                         </div>
                         
                         <div className='col-md-6 listing-details'>
-                        <p className=''>
-                            Lorem ipsum
-                             dolor sit amet consectetur, adipisicing elit.
-                            A, fugiat?
-                        </p>
+                            <h3>
+                                #2,500,000
+                            </h3>
                         </div>
 
                     </div>
@@ -70,6 +67,69 @@ function Listings() {
                     <div>toilets</div>
                 </div>
             </div>
+
+
+
+
+
+
+            <div className='list-grid'>
+            
+            <h3 style={{padding:'0 2%'}}>2 bedroom duplex apartment</h3>
+            <div className='gridProduct row'>
+
+                <div className='list-image col-md-6'>
+                    <img src={image} alt='apartment'/>
+                </div>
+                
+                <div className='col-md-6 listing-details'>
+                    <h3>
+                        #2,500,000
+                    </h3>
+                </div>
+
+            </div>
+
+
+        
+        <div className='list-footer'>
+            <div>bedroom</div>
+            <div>bathrooms</div>
+            <div>toilets</div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <div className='list-grid'>
+            
+            <h3 style={{padding:'0 2%'}}>2 bedroom duplex apartment</h3>
+            <div className='gridProduct row'>
+
+                <div className='list-image col-md-6'>
+                    <img src={image} alt='apartment'/>
+                </div>
+                
+                <div className='col-md-6 listing-details'>
+                    <h3>
+                        #2,500,000
+                    </h3>
+                </div>
+
+            </div>
+
+
+        
+        <div className='list-footer'>
+            <div>bedroom</div>
+            <div>bathrooms</div>
+            <div>toilets</div>
+        </div>
+    </div>
         </div>
         </div>
     </section>
